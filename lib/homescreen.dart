df8331a3970e212a1e432033069726b3cd1f422d
MANISH VERMA <55659257+Verma35@users.noreply.github.com>
2020-09-25 18:06:41 +0530

    first commit

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -9,56 +9,48 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 2;
-  void roll() {
+  void roll(){
     setState(() {
-      leftDiceNumber = Random().nextInt(6) + 1;
-      rightDiceNumber = Random().nextInt(6) + 1;
+      leftDiceNumber=Random().nextInt(6)+1;
+      rightDiceNumber=Random().nextInt(6)+1;
+      
     });
   }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Dicee'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.all(15),
-                    child: Image(
-                      image: AssetImage(
-                          'assets/images/dice-png-$leftDiceNumber.png'),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.all(15),
-                    child: Image(
-                      image: AssetImage(
-                          'assets/images/dice-png-$rightDiceNumber.png'),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            RaisedButton(
-              onPressed: () {
+        appBar: AppBar(
+          title: Text('dicee'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Expanded(          
+                      child: Padding(
+                          padding: EdgeInsets.all(15),
+                          child: Image(
+                              image:
+                                  AssetImage('assets/images/dice-png-$leftDiceNumber.png')))),
+                  Expanded(
+                      child: Padding(
+                          padding: EdgeInsets.all(15),
+                          child: Image(
+                              image:
+                                  AssetImage('assets/images/dice-png-$rightDiceNumber.png'))))
+                ],
+              ),
+              RaisedButton(onPressed: (){
                 roll();
               },
-              child: Text('Roll'),
-              disabledColor: Colors.green,
-              hoverElevation: 5.0,
-            )
-          ],
-        ),
-      ),
-    );
+              child: Text('ROLL'),
+              )
+            ],
+          ),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,16 @@
-import 'package:rolling_Dice/splash.dart';
 import 'package:flutter/material.dart';
+import './homescreen.dart';
 
 void main() {
   runApp(MyApp(),
     
   );
 }
-
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: SplashScreen(),
-    );
+    return MaterialApp(home:HomeScreen());
   }
 }
